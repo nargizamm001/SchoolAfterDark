c0b778b885f9835b2432b1640209aeea218f6b0f
nargizamm001 <nargizamnaidarova@gmail.com>
2026-05-24 22:24:22 +0500

    lvl1

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -1,21 +1,59 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
+    [Header("Win UI")]
+    public GameObject youWinText;
+
+    [Header("Scene Settings")]
+    public string nextLevelName = "Level2";
+    public float delayBeforeNextLevel = 2f;
+
     private bool activated = false;
+
+    void Start()
+    {
+        if (youWinText != null)
+        {
+            youWinText.SetActive(false);
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (activated) return;
-
-        if (other.CompareTag("Player"))
+        if (activated)
         {
-            activated = true;
-
-            if (GameManager.instance != null)
-            {
-                GameManager.instance.WinLevel();
-            }
+            return;
         }
+
+        bool isPlayer =
+            other.CompareTag("Player") ||
+            other.transform.root.CompareTag("Player");
+
+        if (!isPlayer)
+        {
+            return;
+        }
+
+        activated = true;
+        StartCoroutine(WinRoutine());
+    }
+
+    IEnumerator WinRoutine()
+    {
+        if (youWinText != null)
+        {
+            youWinText.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("YouWinText is not assigned in LevelExit.");
+        }
+
+        yield return new WaitForSeconds(delayBeforeNextLevel);
+
+        SceneManager.LoadScene(nextLevelName);
     }
 }
